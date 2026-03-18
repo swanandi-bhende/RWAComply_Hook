4073856dab13af2c19af1dc470aedbd4840470b6
Swanandi Bhende <swanandibhende@gmail.com>
2026-03-18 20:50:34 +0530

    PoolInitialization

--- script/DeployFull.s.sol
+++ script/DeployFull.s.sol
@@ -6,6 +6,10 @@ import "forge-std/Script.sol";
 import {PoolManager} from "@uniswap/v4-core/PoolManager.sol";
 import {IPoolManager} from "@uniswap/v4-core/interfaces/IPoolManager.sol";
 
+import {PoolKey} from "@uniswap/v4-core/types/PoolKey.sol";
+import {Currency} from "@uniswap/v4-core/types/Currency.sol";
+import {IHooks} from "@uniswap/v4-core/interfaces/IHooks.sol";
+
 import "../src/RWAComplyHook.sol";
 import "../src/MockRWAOracle.sol";
 import "../src/MockERC20.sol";
@@ -30,6 +34,44 @@ contract DeployFull is Script {
 
         tokenA.mint(msg.sender, 1e24);
         tokenB.mint(msg.sender, 1e24);
+
+        tokenA.approve(address(poolManager), type(uint256).max);
+        tokenB.approve(address(poolManager), type(uint256).max);
+
+        (address token0, address token1) =
+            address(tokenA) < address(tokenB)
+                ? (address(tokenA), address(tokenB))
+                : (address(tokenB), address(tokenA));
+
+        PoolKey memory key = PoolKey({
+            currency0: Currency.wrap(token0),
+            currency1: Currency.wrap(token1),
+            fee: 3000,
+            tickSpacing: 60,
+            hooks: IHooks(address(hook))
+        });
+
+        uint160 sqrtPriceX96 = 79228162514264337593543950336;
+        poolManager.initialize(key, sqrtPriceX96);
+
+        IPoolManager.ModifyLiquidityParams memory liquidityParams =
+            IPoolManager.ModifyLiquidityParams({
+                tickLower: -600,
+                tickUpper: 600,
+                liquidityDelta: 1000 ether,
+                salt: bytes32(0)
+            });
+
+        poolManager.modifyLiquidity(key, liquidityParams, "");
+
+        IPoolManager.SwapParams memory swapParams =
+            IPoolManager.SwapParams({
+                zeroForOne: true,
+                amountSpecified: int256(1e18),
+                sqrtPriceLimitX96: 0
+            });
+
+        poolManager.swap(key, swapParams, "");
 
         vm.stopBroadcast();
     }
